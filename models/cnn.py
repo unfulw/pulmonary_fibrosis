9902ddb25bd25514b0e14be760d57969e56eee78
unfulw <rlaalswns0816@gmail.com>
2025-11-22 13:59:11 +0800

    working on late fusion

--- models/cnn.py
+++ models/cnn.py
@@ -14,6 +14,7 @@ import torch.nn.functional as F
 from tqdm import tqdm
 import pickle
 
+
 # Add preprocessing module to path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "preprocessing", "scan")))
 from preprocess import preprocess_scans, get_preprocessed_scan
